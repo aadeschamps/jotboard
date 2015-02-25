ab7657497188ab2f6470779b09b9cee301a59375
Alex <alexadeschamps@gmail.com>
2015-02-25 11:53:39 -0500

    sizes dropdouwn working in testing

--- public/js/test.js
+++ public/js/test.js
@@ -1,15 +1,29 @@
 
-var clicked = false;
+var colors_clicked = false;
 $('#colors').click(function(){
-	if( clicked ){
+	if( colors_clicked ){
 		$('#all-colors').css('display', 'none')
-		clicked = false;
+		colors_clicked = false;
 	}else{
 		$('#all-colors').css('display', 'block')
-		clicked = true;
+		colors_clicked = true;
 	}
 })
 
 $('.change-color').click(function(){
+	var id = $(this).attr('id')
+	console.log(id);
+	$('#current-color').removeClass();
+	$('#current-color').addClass('color ' + id);
+})
 
+var size_clicked = false;
+$('#sizes').click(function(){
+	if( size_clicked ){
+		$('#all-sizes').css('display', 'none')
+		size_clicked = false;
+	}else{
+		$('#all-sizes').css('display', 'block')
+		size_clicked = true;
+	}
 })
